7062e8892fe9b8b28a5ac97db1499c666d9ab27c
EkaanshArora <ekaansh68@gmail.com>
2022-12-05 18:58:44 +0530

    feat: day five

---
+++ 5/part2.ts
@@ -0,0 +1,40 @@
+import text from "./problemText";
+
+const textpieces = text.split('\n\n')
+const topText = textpieces[0].split('\n')
+const bottomText = textpieces[1].split('\n')
+const stackCollection: {[index: number]: string[]} = {}
+topText.forEach((linestr, i) => {
+  const line = linestr.split("");
+  if (line[1] !== '1') {
+    for (let index = 0; index < line.length - 2; index += 4) {
+      if (line[index + 1] !== " ") {
+        if (!stackCollection[(index / 4 + 1)]) {
+          stackCollection[(index / 4 + 1)] = [];
+        }
+        stackCollection[(index / 4 + 1)].push(line[index + 1]);
+      }
+    }
+  }
+});
+
+bottomText.forEach(instruction => {
+  const line = instruction.split('')
+  const items = parseInt(line[5] + line[6])
+  let isDoubleDigit = true
+  if(line[6] === ' ') isDoubleDigit = false
+  const from = parseInt(line[isDoubleDigit ? 13 : 12 ])
+  const to = parseInt(line[isDoubleDigit ? 18 : 17])
+
+  let pieces = []
+  for(let i = 0; i < items; i++){
+    pieces.push(stackCollection[from].shift())
+  }
+  pieces.reverse()
+  for(let i = 0; i < pieces.length; i++){
+    stackCollection[to].unshift(pieces[i])
+  }
+})
+
+const answer = (Object.values(stackCollection).map(e=>e[0])).join('')
+console.log(answer)
